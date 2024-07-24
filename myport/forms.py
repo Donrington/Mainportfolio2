89df1afbc3df984f7c9be9a3a39e0aa41b6b0e6d
Donrington <carryoby@mail.com>
2024-07-24 05:12:30 +0100

    admin modified

--- myport/forms.py
+++ myport/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, FileField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional, URL
 from flask_wtf.file import FileAllowed
 
 class BlogPostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     image = FileField('Image (optional)', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Images only!')])
+    image_url = StringField('Image URL', validators=[Optional(), URL()])
     author = StringField('Author', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Post')
